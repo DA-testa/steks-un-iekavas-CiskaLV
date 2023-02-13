aa9e264be27ac7dce1de5ee5bcd6938486e2d3b3
Ciska <44933905+CiskaLV@users.noreply.github.com>
2023-02-13 13:32:36 +0200

    First test lets see if it works

--- main.py
+++ main.py
@@ -14,17 +14,28 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                print("in here")
+                break
             pass
+
+            opening_brackets_stack.pop();
+
+    return opening_brackets_stack
 
 
 def main():
     text = input()
+    if (text[0] != "I"):
+        text = "I" + text
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch[-1].position if mismatch else "Success")
 
 
 if __name__ == "__main__":
